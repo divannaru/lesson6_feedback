a010a36edd670e691071cc7724980456dd73a950
divannaru <43481157+divannaru@users.noreply.github.com>
2018-11-12 12:49:10 +0300

    Update msgshow.php
    Исправлена опечатка в ORDR и перенесен вывод сообщений в то место, где они формируются.

--- msgshow.php
+++ msgshow.php
@@ -2,19 +2,19 @@
 
 include "config.php";
 
-$sql = "select * from messages OPDER BY time DESC LIMIT 5";
+$sql = "select * from messages ORDER BY time DESC LIMIT 5";
 $result = mysqli_query($connect, $sql);
 $num = mysqli_num_rows($result);
 
 if ($num >0){
     while ($data=mysqli_fetch_assoc($result)){
         $msg='<p><span class="message">'.$data[message].'</span></i><br><span class="username">'.$data[username].'</span></p>';
+        echo $msg;
     }
 }
 else {
     $msg="<p>Отзывов пока нет</p>";
+    echo $msg;
 }
-echo $msg;
-//mysqli_close($connect);
-//return;
+
 ?>
